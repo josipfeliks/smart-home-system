68e2ab9aca8907861b455d6f974a6c3bf7495e95
Josip Feliks <josip.feliks89@gmail.com>
2022-10-21 00:38:47 +0200

    rename ICurtainsService

--- smart-home-system-api/src/main/java/com/example/smarthomesystemapi/services/interfaces/ICurtainsService.java
+++ smart-home-system-api/src/main/java/com/example/smarthomesystemapi/services/interfaces/ICurtainsService.java
@@ -3,7 +3,7 @@ package com.example.smarthomesystemapi.services.interfaces;
 import java.time.LocalTime;
 import java.util.UUID;
 
-public interface ICurtainService {
+public interface ICurtainsService {
 
     boolean openCurtains(UUID curtainsId, LocalTime when);
     boolean openCurtains(UUID curtainsId);
